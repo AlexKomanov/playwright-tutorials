cb224220b240b5cb22a7b8fd9c8d92e385829c48
AlexKomanov <akomanov88@gmail.com>
2022-06-25 16:11:25 +0300

    Modified Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import './home.scss'
 
 const Home = () => {
     return (
         <div className='home'>
-            Home
+           
+            <Sidebar />
+
         </div>
     )
 }
