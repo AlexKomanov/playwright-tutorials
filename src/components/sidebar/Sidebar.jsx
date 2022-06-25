84795a10034d86c2f011e432c9963a1c06842809
AlexKomanov <akomanov88@gmail.com>
2022-06-25 16:11:05 +0300

    Added new components

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./sidebar.scss"
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="top">
+            <span className="logo">Playwright Tutorials</span>
+        </div>
+        <div className="center">
+
+        </div>
+        <div className="bottom">
+            color options
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
