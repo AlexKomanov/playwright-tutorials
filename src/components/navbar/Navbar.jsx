5e6106e48e5734d28b098ac56fcaf3dbdbab0260
AlexKomanov <akomanov88@gmail.com>
2022-06-26 23:12:54 +0300

    Start of the Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,40 @@
+import { ChatBubbleOutlineOutlined, DarkModeOutlined, FullscreenExitOutlined, LanguageOutlined, ListOutlined, NotificationsNoneOutlined, SearchSharp } from '@mui/icons-material';
 import React from 'react'
 import "./navbar.scss";
 
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="searh">
+          <input type="text" placeholder='Search...' />
+          <SearchSharp />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlined />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined />
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined />
+          </div>
+          <div className="item">
+            <ListOutlined />
+          </div>
+        </div>
+        </div>      
+      
+      </div>
   )
 }
 
