4a4dc83abf7e77bdc9cc5e1a28398ed7957c1dc5
AlexKomanov <akomanov88@gmail.com>
2022-06-26 18:18:54 +0300

    Adding additional sections and styles to the sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,27 +1,70 @@
 import "./sidebar.scss";
+import { InstallDesktopSharp, Keyboard, MouseOutlined, SchoolRounded, TextFieldsOutlined } from '@mui/icons-material'
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className="top">
-                <img className='pl-logo' src="https://playwright.dev/img/playwright-logo.svg" alt="playwright-logo"/>
+                <img className='pl-logo' src="https://playwright.dev/img/playwright-logo.svg" alt="playwright-logo" />
                 <span className="logo">Learn Playwright</span>
             </div>
             <hr />
             <div className="center">
                 <ul>
+                    <p className="title">INSTALLATION</p>
                     <li>
-                        <span>Dashboard</span>
+                        <InstallDesktopSharp className="icon" />
+                        <span>Getting Started</span>
+                    </li>
+                    <p className="title">BASIC ACTIONS</p>
+                    <li>
+                        <MouseOutlined className="icon" />
+                        <span>Click Element</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                    <Keyboard className="icon"/>
+                        <span>Type Text</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                    <TextFieldsOutlined className="icon"/>
+                        <span>Read Text</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                    <SchoolRounded className="icon"/>
+                        <span>Lorem Ipsum</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                    <SchoolRounded className="icon"/>
+                        <span>Lorem Ipsum</span>
                     </li>
+                    <li>
+                    <SchoolRounded className="icon"/>
+                        <span>Lorem Ipsum</span>
+                    </li>
+                    <p className="title">SETTINGS</p>
+                    <li>
+                    <SchoolRounded className="icon"/>
+                        <span>Configuration</span>
+                    </li>
+                    <li>
+                        <SchoolRounded className="icon"/>
+                        <span>Lorem Ipsum</span>
+                    </li>
+                    <li>
+                        <SchoolRounded className="icon"/>
+                        <span>Lorem Ipsum</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <SchoolRounded className="icon"/>
+                        <span>Lorem Ipsum</span>
+                    </li>
+                    <li>
+                        <SchoolRounded className="icon"/>
+                        <span>Lorem Ipsum</span>
+                    </li>
+
                 </ul>
             </div>
             <div className="bottom">
