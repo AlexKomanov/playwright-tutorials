cbabf34caf1b2d548869d5990195ff98164a328e
AlexKomanov <akomanov88@gmail.com>
2022-06-27 06:18:50 +0300

    Adding additional styles and items to Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,29 +7,37 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="wrapper">
-        <div className="searh">
+        <div className="search">
           <input type="text" placeholder='Search...' />
           <SearchSharp />
         </div>
         <div className="items">
           <div className="item">
-            <LanguageOutlined />
+            <LanguageOutlined className='icon'/>
             English
           </div>
           <div className="item">
-            <DarkModeOutlined />
+            <DarkModeOutlined className='icon'/>
           </div>
           <div className="item">
-            <FullscreenExitOutlined />
+            <FullscreenExitOutlined className='icon'/>
           </div>
           <div className="item">
-            <NotificationsNoneOutlined />
+            <NotificationsNoneOutlined className='icon'/>
+            <div className="counter">5</div>
           </div>
           <div className="item">
-            <ChatBubbleOutlineOutlined />
+            <ChatBubbleOutlineOutlined className='icon'/>
+            <div className="counter">1</div>
           </div>
           <div className="item">
-            <ListOutlined />
+            <ListOutlined className='icon'/>
+          </div>
+          <div className="item">
+            <img src="https://cdn.pixabay.com/photo/2016/06/28/01/42/chess-1483735_960_720.jpg" 
+            alt="avatar" 
+            className="avatar"
+            />
           </div>
         </div>
         </div>      
